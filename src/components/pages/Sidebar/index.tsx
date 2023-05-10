dff8e9e051aadaf8b8e54784ff455b35aee05c96
kwonkangbin <kbk282655@gmail.com>
2023-05-10 15:56:29 +0900

    ADD : Sidebar

---
+++ src/components/pages/Sidebar/index.tsx
@@ -0,0 +1,52 @@
+import * as S from './style';
+import * as I from '@assets/images/allfie';
+
+export const Sidebar = () => {
+	return (
+		<S.Bar>
+			<S.User>
+				<img src={I.UserImg} alt="유저사진" />
+				<span>UserName</span>
+			</S.User>
+			<S.Line />
+			<S.Menu>
+				<S.Subtitle>Menu</S.Subtitle>
+				<S.Option to="/">
+					<S.Icon src={I.Menu} alt="메인아이콘" />
+					<S.Pagename>메인</S.Pagename>
+				</S.Option>
+				<S.Option to="">
+					<S.Icon src={I.BulletinBoard} alt="게시판아이콘" />
+					<S.Pagename>게시판</S.Pagename>
+				</S.Option>
+				<S.Option to="">
+					<S.Icon src={I.Computer} alt="칠판아이콘" />
+					<S.Pagename>기술 블로그</S.Pagename>
+				</S.Option>
+				<S.Option to="">
+					<S.Icon src={I.Trophy} alt="랭킹 아이콘" />
+					<S.Pagename>랭킹</S.Pagename>
+				</S.Option>
+			</S.Menu>
+			<S.Menu>
+				<S.Subtitle>User</S.Subtitle>
+				<S.Option to="">
+					<S.Icon src={I.Alarm} alt="알람 아이콘" />
+					<S.Pagename>알람</S.Pagename>
+				</S.Option>
+				<S.Option to="">
+					<S.Icon src={I.UserIcon} alt="마이페이지 아이콘" />
+					<S.Pagename>마이페이지</S.Pagename>
+				</S.Option>
+				<S.Option to="">
+					<S.Icon src={I.Setting} alt="설정 아이콘" />
+					<S.Pagename>설정</S.Pagename>
+				</S.Option>
+				<S.Option to="">
+					<S.Icon src={I.Infomation} alt="소개 아이콘" />
+					<S.Pagename>개발자 소개</S.Pagename>
+				</S.Option>
+			</S.Menu>
+		</S.Bar>
+	);
+};
---
+++ src/components/pages/Sidebar/style.ts
@@ -0,0 +1,109 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import {Body, Body2} from '@styles/text.style';
+
+export const Bar = styled.nav`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 20px;
+	gap: 20px;
+
+	position: absolute;
+	width: 240px;
+	height: 100vh;
+	left: 0px;
+	top: 0px;
+
+	background: #EFF4F4;
+`;
+
+export const User = styled(Body2)`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 10px;
+	gap: 10px;
+
+	width: 200px;
+	height: 56px;
+
+	border-radius: 8px;
+
+	/* Inside auto layout */
+
+	flex: none;
+	order: 0;
+	align-self: stretch;
+	flex-grow: 0;
+`;
+
+export const Line = styled.hr`
+	width: 200px;
+	height: 0px;
+
+	border: 1px solid #E1E8EA;
+`;
+
+export const Menu = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 0px;
+
+	width: 200px;
+	height: 202px;
+
+
+	/* Inside auto layout */
+
+	flex: none;
+	order: 2;
+	align-self: stretch;
+	flex-grow: 0;
+`;
+
+export const Subtitle = styled(Body)`
+	display: flex;
+	flex-direction: row;
+	align-items: flex-start;
+	padding: 0px 0px 10px;
+
+	color: #7a8184;
+`;
+
+export const Option = styled(Link)`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 10px;
+	gap: 10px;
+
+	width: 200px;
+	height: 44px;
+
+	border-radius: 8px;
+	text-decoration: none;
+`;
+
+export const Icon = styled.img`
+	/* Computer */
+	width: 24px;
+	height: 24px;
+`;
+
+export const Pagename = styled(Body2)`
+	font-style: normal;
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 17px;
+
+	/* Gray Colors/Gray Dark 1 */
+	color: #7a8184;
+
+	/* Inside auto layout */
+
+	flex: none;
+	order: 1;
+	flex-grow: 0;
+`;
