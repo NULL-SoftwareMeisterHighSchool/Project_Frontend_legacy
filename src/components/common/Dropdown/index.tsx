bd7ed44bce17c18123527fb9c0dc7ad6e9e1b711
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-05-20 17:23:07 +0900

    ADD : Dropdown 디자인 시스템 제작

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -1,9 +1,46 @@
-import * as S from './style';
+import { useState } from "react";
+import { ItemType } from "./dropdown.type";
+import * as S from "./style";
+import { DownArrow } from "@assets/images/allfiles";
 
-const Dropdown = () => {
-    return (
-        <S.Dropdown></S.Dropdown>
-    )
+interface PropTypes {
+  describe: string;
+  items: ItemType[];
 }
+
+const Dropdown = ({ describe, items }: PropTypes) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<ItemType>();
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const clickItem = (item: ItemType) => {
+    setSelectedItem(item);
+    toggleOpen();
+  };
+
+  return (
+    <S.Dropdown>
+      <S.DropdownBox onClick={toggleOpen}>
+        <S.Describe>{selectedItem?.text ? selectedItem.text : describe}</S.Describe>
+        <S.Img src={DownArrow} />
+      </S.DropdownBox>
+      <S.DropdownListBox isOpen={isOpen}>
+        <S.DropdownList>
+          {items.map((item: ItemType, idx) => (
+            <S.DropdownItem
+              key={"dropdown " + idx}
+              onClick={() => clickItem(item)}
+            >
+              {item.text}
+            </S.DropdownItem>
+          ))}
+        </S.DropdownList>
+      </S.DropdownListBox>
+    </S.Dropdown>
+  );
+};
 
 export default Dropdown;
