fadb34d0ea257581717b9b660edb1bf02ef2e13e
kwonkangbin <kbk282655@gmail.com>
2023-05-22 01:01:42 +0900

    CHORE : self-closing 태그 적용

--- src/components/common/Sidebar/index.tsx
+++ src/components/common/Sidebar/index.tsx
@@ -12,17 +12,17 @@ export const Sidebar = () => {
 			<S.Line />
 			<S.Menu>
 				<S.Subtitle>Menu</S.Subtitle>
-				<Option to='/' src={I.Menu} pagename='메인'></Option>
-				<Option to='/' src={I.BulletinBoard}  pagename='게시판'></Option>
-				<Option to='/' src={I.Computer} pagename='기술 블로그'></Option>
-				<Option to='/' src={I.Trophy}  pagename='랭킹'></Option>
+				<Option to='/' src={I.Menu} pagename='메인' />
+				<Option to='/' src={I.BulletinBoard}  pagename='게시판' />
+				<Option to='/' src={I.Computer} pagename='기술 블로그' />
+				<Option to='/' src={I.Trophy}  pagename='랭킹' />
 			</S.Menu>
 			<S.Menu>
 				<S.Subtitle>User</S.Subtitle>
-				<Option to='/' src={I.Alarm}  pagename='알림'></Option>
-				<Option to='/' src={I.UserIcon}  pagename='마이페이지'></Option>
-				<Option to='/' src={I.Setting}  pagename='설정'></Option>
-				<Option to='/' src={I.Infomation}  pagename='개발자 소개'></Option>
+				<Option to='/' src={I.Alarm}  pagename='알림' />
+				<Option to='/' src={I.UserIcon}  pagename='마이페이지' />
+				<Option to='/' src={I.Setting}  pagename='설정' />
+				<Option to='/' src={I.Infomation}  pagename='개발자 소개' />
 			</S.Menu>
 		</S.Bar>
 	);
