7c0a1f51e888e465ef1e779a4610f93920b7916e
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-05-22 00:23:21 +0900

    Fix : App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import Dropdown from "@components/common/Dropdown";
-
 function App() {
-  return <div className="App">
-    <Dropdown describe="옵션을 선택하세요." items={[
-      { text: "옵션1" },
-      { text: "옵션2" },
-      { text: "옵션3" }
-    ]} />
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
