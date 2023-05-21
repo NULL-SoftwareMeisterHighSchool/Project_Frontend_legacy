fb8eb1903ab55532f1c819de68c18cf4fbf72f6f
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-05-22 02:31:29 +0900

    ADD : AppLayout 제작

---
+++ src/layouts/AppLayout/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+`;
+
+export const Content = styled.div`
+  padding: 60px 200px;
+
+  display: flex;
+  flex-direction: column;
+
+  overflow-y: scroll;
+`;
