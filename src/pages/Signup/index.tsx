f820828dea589a39573f7361ff0fb32ac81be4a1
kimgyuha <kimgyuha25@gmail.com>
2023-05-30 13:10:52 +0900

    ADD : signup 페이지 2개 추가

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,56 +1,66 @@
-import React from "react";
-import LeftArrow from "@assets/images/pages/LeftArrow.svg";
-import LoginIcon from "@assets/images/pages/LoginIcon.svg";
-import LoginImg from "@assets/images/pages/LoginImg.jpg";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import * as S from "./style";
 
+import Button from "@components/common/Button";
+import Input from "@components/common/Input";
+import SignupLayout from "@layouts/SignupLayout";
+
+import { Body2, BodyLarge, BodyStrong, TitleLarge } from "@styles/text.style";
+import LeftArrow from "@assets/images/pages/LeftArrow.svg";
+type ArrayType = {
+	name: string, title: string, value: string, placeholder: string
+}
 const Signup = () => {
+	const [pageNum, setPageNum] = useState(1);
+
+	const [userData, setUserData] = useState({
+		school: "",
+		email: "",
+		year: "",
+		name: "",
+		id: "",
+		password: ""
+	});
+	const inputType: ArrayType[] = [
+		{ name: "school", title: "학교", value: "", placeholder: "재학중이거나 졸업한 학교를 입력해 주세요" },
+		{ name: "email", title: "학교 이메일", value: "", placeholder: "학교 전용 이메일을 입력해 주세요" },
+		{ name: "year", title: "입학연도", value: "", placeholder: "학교에 입학한 년도를 입력해 주세요" },
+		{ name: "name", title: "이름", value: "", placeholder: "실명을 입력해 주세요" },
+		{ name: "id", title: "아이디", value: "", placeholder: "아이디를 입력해 주세요" },
+		{ name: "password", title: "비밀번호", value: "", placeholder: "영·숫자·기호 포함 8자 이상" }
+	];
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setUserData({
+			...userData,
+			[name]: value
+		})
+	}
+
 	return (
-		<S.Container>
-			<S.LoginImage src={LoginImg}>
-				<img src={LoginIcon} />
-			</S.LoginImage>
-			<S.SignupForm>
-				<S.Wrapper>
-					<S.Title>
-						<S.Subtitle>Sign up</S.Subtitle>
-						<S.Prev>
-							<Link to="/Login">
-								<S.PrevBtn src={LeftArrow} />
-							</Link>
-							회원가입
-						</S.Prev>
-					</S.Title>
-					<S.Inputs>
-						<S.Name>
-							<S.Subtitle>이름</S.Subtitle>
-							<S.InputBox>
-								<S.Input placeholder="실명을 입력해 주세요" />
-							</S.InputBox>
-						</S.Name>
-						<S.Id>
-							<S.Subtitle>아이디</S.Subtitle>
-							<S.InputBox>
-								<S.Input placeholder="아이디를 입력해 주세요" />
-							</S.InputBox>
-						</S.Id>
-						<S.Password>
-							<S.Subtitle>비밀번호</S.Subtitle>
-							<S.InputBox>
-								<S.Input placeholder="영·숫자·기호 포함 8자 이상" />
-							</S.InputBox>
-						</S.Password>
-					</S.Inputs>
-					<S.Auth>
-						<S.Subtitle>
-							회원이신가요? <Link to="/Login">로그인</Link>
-						</S.Subtitle>
-						<S.SignupBtn>회원가입</S.SignupBtn>
-					</S.Auth>
-				</S.Wrapper>
-			</S.SignupForm>
-		</S.Container>
+		<SignupLayout >
+			<div>
+				<S.Title>
+					<BodyLarge>Sign In</BodyLarge>
+					<S.ArrowContainer>
+						{pageNum !== 1 && <S.BackArrow src={LeftArrow} />}
+						<TitleLarge>회원가입</TitleLarge>
+					</S.ArrowContainer>
+				</S.Title>
+				<S.InputContainer>
+					{
+						inputType.slice((pageNum - 1) * 3, pageNum * 3).map(d => <Input title={d.title} width="100%" placeholder={d.placeholder} name={d.name} onChange={onChange} />)
+					}
+				</S.InputContainer>
+				<S.SubmitContainer>
+					<div>
+						<Body2>회원이신가요?</Body2>
+						<BodyStrong>로그인</BodyStrong>
+					</div>
+					<Button height="48px" value={pageNum === 1 ? "다음" : "회원가입"} />
+				</S.SubmitContainer>
+			</div>
+		</SignupLayout>
 	);
 };
 
