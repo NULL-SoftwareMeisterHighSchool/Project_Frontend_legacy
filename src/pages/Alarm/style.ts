7c87aea7413da6526d619cca963cc43cbf04315d
kwonkangbin <kbk282655@gmail.com>
2023-07-02 23:53:37 +0900

    ADD : 알림페이지 반응형 적용

--- src/pages/Alarm/style.ts
+++ src/pages/Alarm/style.ts
@@ -5,13 +5,14 @@ import { Body2, TitleLarge, BodyStrong } from "@styles/text.style";
 export const AlarmContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   gap: 40px;
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  width: 1280px;
+  width: 100%;
   height: 530px;
 `;
 
