e6499a1f735721a11ba27497ea69824b43cc4fc5
kimgyuha <kimgyuha25@gmail.com>
2023-07-24 19:00:29 +0900

    ADD : 회원탈퇴 모달

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -7,10 +7,25 @@ import { color } from "@styles/theme.style";
 import { Lock } from "@assets/images/icon/Lock";
 import { Mail } from "@assets/images/icon/Mail";
 import { RemoveCircle } from "@assets/images/icon/RemoveCircle";
+import { useState } from "react";
+import Modal from "@components/common/modal";
 
 const Setting = () => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
     return (
         <AppLayout title="설정" path="User > 설정">
+            <Modal val={modalOpen} setVal={setModalOpen}>
+                <S.UseTitleContainer>
+                    <S.UserTitle>정말로 탈퇴하실건가요?</S.UserTitle>
+                    <S.UserSubTitle>
+                        탈퇴하면 지금까지 활동한 기록들을 복구할 수 없어요.
+                    </S.UserSubTitle>
+                </S.UseTitleContainer>
+                <S.UserBtnContainer>
+                    <button>취소</button>
+                    <button>회원 탈퇴 하기</button>
+                </S.UserBtnContainer>
+            </Modal>
             <S.Container>
                 <UserSetting
                     title="비밀번호 변경"
@@ -18,7 +33,7 @@ const Setting = () => {
                         <Lock width={24} height={24} fill={color.primaryBase} />
                     }
                     colorType={color.black}
-                    onClick={()=>{}}
+                    onClick={() => {}}
                 />
                 <UserSetting
                     title="이메일 변경"
@@ -50,7 +65,7 @@ const Setting = () => {
                         />
                     }
                     colorType={color.critical}
-                    onClick={() => {}}
+                    onClick={() => setModalOpen(true)}
                 />
             </S.Container>
         </AppLayout>
