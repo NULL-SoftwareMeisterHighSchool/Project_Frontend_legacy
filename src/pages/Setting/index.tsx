fc6079897ccf943878f2c389036f7dccbf50e4a4
kimgyuha <kimgyuha25@gmail.com>
2023-07-24 12:45:19 +0900

    ADD : 설정 추가

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -2,23 +2,59 @@ import * as S from "./style";
 
 import UserSetting from "@components/pages/Setting/UserSetting.tsx";
 import AppLayout from "@layouts/AppLayout";
-import { User } from "@assets/images/icon/User";
 import { Infomation } from "@assets/images/icon/Infomation";
 import { color } from "@styles/theme.style";
 import { Lock } from "@assets/images/icon/Lock";
 import { Mail } from "@assets/images/icon/Mail";
+import { RemoveCircle } from "@assets/images/icon/RemoveCircle";
 
 const Setting = () => {
     return (
         <AppLayout title="설정" path="User > 설정">
             <S.Container>
-                <UserSetting title="프로필 수정" path="/" icon={<User width={24} height={24} fill={color.primaryBase} />}/>
-                <UserSetting title="비밀번호 변경" path="/" icon={<Lock width={24} height={24} fill={color.primaryBase} />}/>
-                <UserSetting title="이메일 변경" path="/" icon={<Mail width={24} height={24} fill={color.primaryBase} />}/>
-                <UserSetting title="개인정보 처리방침" path="/" icon={<Infomation width={24} height={24} fill={color.grayDark1} />} colorType/>
+                <UserSetting
+                    title="비밀번호 변경"
+                    icon={
+                        <Lock width={24} height={24} fill={color.primaryBase} />
+                    }
+                    colorType={color.black}
+                    onClick={()=>{}}
+                />
+                <UserSetting
+                    title="이메일 변경"
+                    icon={
+                        <Mail width={24} height={24} fill={color.primaryBase} />
+                    }
+                    colorType={color.black}
+                    onClick={() => {}}
+                />
+                <UserSetting
+                    title="개인정보 처리방침"
+                    icon={
+                        <Infomation
+                            width={24}
+                            height={24}
+                            fill={color.grayDark1}
+                        />
+                    }
+                    colorType={color.grayDark1}
+                    onClick={() => {}}
+                />
+                <UserSetting
+                    title="회원 탈퇴"
+                    icon={
+                        <RemoveCircle
+                            width={24}
+                            height={24}
+                            fill={color.critical}
+                        />
+                    }
+                    colorType={color.critical}
+                    onClick={() => {}}
+                />
             </S.Container>
         </AppLayout>
-    )
-}
+    );
+};
 
 export default Setting;
