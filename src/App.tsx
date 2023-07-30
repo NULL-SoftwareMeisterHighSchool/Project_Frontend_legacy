eb1c5ae6256a65b5df3e1ee5103089eb1d720898
kimgyuha <kimgyuha25@gmail.com>
2023-07-30 22:28:04 +0900

    ADD : router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
+import Alarm from "@pages/Alarm";
+import Board from "@pages/Board";
 import BoardDetail from "@pages/BoardDetail";
+import EnRolledSignup from "@pages/EnRolledSignup";
+import GraduateSignup from "@pages/GraduateSignup";
+import Login from "@pages/Login";
+import Main from "@pages/Main";
+import Mypage from "@pages/Mypage";
+import Password from "@pages/Password";
+import Ranking from "@pages/Ranking";
+import Setting from "@pages/Setting";
+import SkillBlog from "@pages/SkillBlog";
+import { createBrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-  <div className="App"><BoardDetail /></div>
-  );
-}
-
-export default App;
+const app = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+    },
+    {
+        path: "/graduatesign",
+        element: <GraduateSignup />,
+    },
+    {
+        path: "/enrollsign",
+        element: <EnRolledSignup />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/mypage",
+        element: <Mypage />,
+    },
+    {
+        path: "/ranking",
+        element: <Ranking />,
+    },
+    {
+        path: "/alarm",
+        element: <Alarm />,
+    },
+    {
+        path: "/blog",
+        element: <Board />,
+    },
+    {
+        path: "/skill",
+        element: <SkillBlog />,
+    },
+    {
+        path: "/blogdetail",
+        element: <BoardDetail />,
+    },
+    {
+        path: "/setting",
+        element: <Setting />,
+    },
+    {
+        path: "/*",
+        element: <p>404</p>,
+    },
+]);
+export default app;
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "@styles/global.style";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 
-if(process.env.NODE_ENV === 'development'){
-  const {worker} = await import('./mocks/browser');
-  worker.start();
+if (process.env.NODE_ENV === "development") {
+    const { worker } = await import("./mocks/browser");
+    worker.start();
 }
 
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>
+    <>
+        <GlobalStyle />
+        <RouterProvider router={App} />
+    </>
 );
