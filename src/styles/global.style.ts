1aaf3e4f1426302b3123828588ed6064e5b1286c
kwonkangbin <kbk282655@gmail.com>
2023-07-31 15:35:48 +0900

    CHORE : CircularProgressbar 글로벌스타일 적용

--- src/styles/global.style.ts
+++ src/styles/global.style.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import { color } from "./theme.style";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
@@ -20,6 +21,20 @@ const GlobalStyle = createGlobalStyle`
     input:focus, button:focus {
         outline: none;
     }
+
+    .CircularProgressbar {
+		width: 120px;
+		height: 120px;
+	}
+
+	.CircularProgressbar-text {
+		font-family: 'Pretendard';
+		font-style: normal;
+		font-weight: 600;
+		font-size: 24px;
+		line-height: 36px;
+		color: ${color.primaryBase};
+	 }
 `;
 
 export default GlobalStyle;
