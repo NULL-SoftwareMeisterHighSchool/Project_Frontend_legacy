f6b1c10635e3dcc95d08bcbe3f3a18dc2c372a7a
kimgyuha <kimgyuha25@gmail.com>
2023-07-31 20:17:50 +0900

    ADD : layout 수정

--- src/layouts/AppLayout/style.ts
+++ src/layouts/AppLayout/style.ts
@@ -1,32 +1,18 @@
 import styled from "styled-components";
-import { color } from "@styles/theme.style";
-import { Body2, TitleLarge } from "@styles/text.style";
 
 export const Container = styled.div`
-  height: 100vh;
-  display: flex;
+    height: 100vh;
+    display: flex;
 `;
 
 export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  padding: 60px 200px;
-  margin-left: 240px;
-  gap: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 60px 200px;
+    margin-left: 240px;
+    gap: 40px;
 
-  overflow-y: scroll;
+    overflow-y: scroll;
 `;
-
-export const TitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-width: max-content ;
-  gap: 22px;
-`;
-export const Path = styled(Body2)<{ primaryBase?: boolean }>`
-  color: ${(props) => props.primaryBase && color.primaryBase};
-`;
-export const Title = styled(TitleLarge)``;
