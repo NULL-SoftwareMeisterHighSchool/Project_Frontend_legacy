83f0fce61e16bd1f0fbf5e9162c1b77364c61c75
kimgyuha <kimgyuha25@gmail.com>
2023-08-01 12:00:46 +0900

    ADD : student login api 코드 작성

--- src/apis/auth/auth.type.ts
+++ src/apis/auth/auth.type.ts
@@ -1,14 +1,51 @@
+import {
+    postSendEmailProps,
+    postSignupStudentProps,
+    postVerifyProps,
+} from "./index";
 import axios from "axios";
 import { postLoginProps } from ".";
 
 const BASE_URL = `${import.meta.env.VITE_BASEURL}/auth`;
 
 export const postLogin = async ({ id, password }: postLoginProps) => {
-
     const resPostLogin = await axios.post(`${BASE_URL}/login`, {
         id,
         password,
     });
 
     return resPostLogin;
+};
+
+export const postSendEmail = async ({ email }: postSendEmailProps) => {
+    const resPostSendEmail = await axios.post(`${BASE_URL}/send-mail`, {
+        email,
+    });
+
+    return resPostSendEmail;
+};
+
+export const postVerify = async ({ email, code }: postVerifyProps) => {
+    const resPostVerify = await axios.post(`${BASE_URL}/verify`, {
+        email,
+        code,
+    });
+    return resPostVerify;
+};
+
+export const postSignupStudent = async ({
+    school,
+    email,
+    admissionYear,
+    name,
+    userID,
+    password,
+    githubID,
+}: postSignupStudentProps) => {
+    const resPostSignupStudent = await axios.post(
+        `${BASE_URL}/signup/student`,
+        { school, email, admissionYear, name, userID, password, githubID }
+    );
+
+    return resPostSignupStudent;
 };
--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -2,3 +2,22 @@ export interface postLoginProps {
     id: string;
     password: string;
 }
+
+export interface postSendEmailProps {
+    email: string;
+}
+
+export interface postVerifyProps {
+    email: string;
+    code: string;
+}
+
+export interface postSignupStudentProps {
+    school: string;
+    email: string;
+    admissionYear: number;
+    name: string;
+    userID: string;
+    password: string;
+    githubID: string;
+}
