8b13bdbccad44ab7e28fcaa72aef00cc4feb4310
kimgyuha <kimgyuha25@gmail.com>
2023-08-02 01:08:47 +0900

    ADD : skillblog api 연동

--- src/apis/article/type.ts
+++ src/apis/article/type.ts
@@ -26,3 +26,31 @@ type CommentType = {
     content: string;
     createdAt: string;
 }
+
+type skillDataProps = {
+    article: blogType[];
+    total: number;
+};
+type blogType = {
+    id: number;
+    type: string;
+    title: string;
+    thumbnail: string;
+    summary: string;
+    author: {
+        id: number;
+        name: string;
+    };
+    createdAt: string;
+};
+export type getBlogProps = {
+    type: string;
+    authorID?: string;
+    duration_start?: string;
+    duration_end?: string;
+    query?: string;
+    offset: number;
+    limit: number;
+    order: string;
+    setData: React.Dispatch<React.SetStateAction<skillDataProps>>;
+};
