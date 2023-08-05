17b234dd895acc218928d35ba369af3ff9df73af
kwonkangbin <kbk282655@gmail.com>
2023-08-05 18:47:06 +0900

    ADD : type 정의

--- src/apis/users/users.type.ts
+++ src/apis/users/users.type.ts
@@ -2,3 +2,15 @@ export type patchEditPasswordProps = {
     currentPassword: string;
     newPassword: string;
 };
+
+export type getGitProps = {
+    setGitData: React.Dispatch<React.SetStateAction<{
+        contributionCount: number;
+        starCount: number;
+        issueCount: number;
+        pullRequestCount: number;
+        contributedRepositoryCount: number;
+        score: number;
+    }>
+    >;
+};
