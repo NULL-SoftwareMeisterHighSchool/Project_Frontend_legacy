0eb3bb490eab2c087b119696c975e5704e1c5188
kwonkangbin <kbk282655@gmail.com>
2023-08-05 18:47:16 +0900

    ADD : 깃허브 API 적용

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -8,28 +8,46 @@ import Post from "@components/common/Post";
 import Banner from "@components/pages/Main/Banner";
 import Record from "@components/pages/Main/Record";
 import Dummy from "@fixtures/board.json";
+import { useQuery } from "react-query";
+import { getGit } from "@apis/users";
 import { useState } from "react";
 import * as S from "./style";
 import TitlePath from "@components/common/TitlePath";
 
 const Main = () => {
-    const [mainData, serMainData] = useState({
-        value: 10,
-        grade: "B+",
-        stars: 11,
-        commit: 1200,
-        pr: 100,
-        issues: 120,
-        contributed: 12,
+    let gitGrade = '';
+    const [gitData, setGitData] = useState({
+        contributionCount: 100,
+        starCount: 12,
+        issueCount: 13,
+        pullRequestCount: 14,
+        contributedRepositoryCount: 15,
+        score: 110
     });
+    const { } = useQuery("userGit", () => getGit({ setGitData }));    
+    if(gitData.score >= 10000){
+        gitGrade = 'S';
+    }else if(gitData.score >= 5000) {
+        gitGrade = 'A';
+    }else if(gitData.score >= 2000) {
+        gitGrade = 'B';
+    }else if(gitData.score >= 800) {
+        gitGrade = 'C';
+    }else if(gitData.score >= 200) {
+        gitGrade = 'D';
+    }else if(gitData.score >= 50) {
+        gitGrade = 'E';
+    }else{
+        gitGrade = 'F';
+    }
     return (
         <>
             <TitlePath title="메인" path="Menu > 메인" primaryBase />
             <S.Github>
                 <S.Score>
                     <CircularProgressbarWithChildren
-                        value={mainData.value}
-                        text={mainData.grade}
+                        value={gitData.score}
+                        text={gitGrade}
                         styles={buildStyles({
                             // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
                             strokeLinecap: "butt",
@@ -49,13 +67,13 @@ const Main = () => {
                     </S.CircularText>
                 </S.Score>
                 <S.Comprehensive>
-                    <Record title="Total Stars Earned" score={mainData.stars} />
-                    <Record title="Total Commits" score={mainData.commit} />
-                    <Record title="Total PRs" score={mainData.pr} />
-                    <Record title="Total Issues" score={mainData.issues} />
+                    <Record title="Total Stars Earned" score={gitData.starCount} />
+                    <Record title="Total Commits" score={gitData.contributionCount} />
+                    <Record title="Total PRs" score={gitData.pullRequestCount} />
+                    <Record title="Total Issues" score={gitData.issueCount} />
                     <Record
                         title="Contributed to"
-                        score={mainData.contributed}
+                        score={gitData.contributedRepositoryCount}
                     />
                 </S.Comprehensive>
             </S.Github>
