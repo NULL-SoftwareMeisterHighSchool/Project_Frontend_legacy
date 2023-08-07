9a614606d53afe4d106124f0fdb4c682a95c6a8c
kimgyuha <kimgyuha25@gmail.com>
2023-08-07 10:57:19 +0900

    FIX : api 연동 에러 수정

--- src/pages/SkillBlog/index.tsx
+++ src/pages/SkillBlog/index.tsx
@@ -7,7 +7,70 @@ import { SkillBlogDefaultImg } from "@assets/images/allfiles";
 import TitlePath from "@components/common/TitlePath";
 
 const SkillBlog = () => {
-    const blogData = skilldata.post;
+    /** skill blog 데이터 */
+    const [skillData, setSkillData] = useState<skillDataProps>({
+        article: [],
+        total: 0,
+    });
+    /** 검색어 */
+    const [searchInput, setSearchInput] = useState<string>("");
+    /** 필터 */
+    const [filterData, setFilterData] = useState("최신순");
+
+    /** axios 연동 함수 */
+    const getSkillData = (newData?: boolean) => {
+        if (newData) {
+            getBlog({
+                type: "TECH",
+                offset: skillData.article.length,
+                limit: 20,
+                order:
+                    filterData === "최신순"
+                        ? "TIME"
+                        : filterData === "최신순"
+                        ? "VIEWS"
+                        : "LIKES",
+                setData: setSkillData,
+                query: searchInput,
+                data: skillData,
+            });
+        } else {
+            getBlog({
+                type: "TECH",
+                offset: 0,
+                limit: 20,
+                order:
+                    filterData === "최신순"
+                        ? "TIME"
+                        : filterData === "최신순"
+                        ? "VIEWS"
+                        : "LIKES",
+                setData: setSkillData,
+                data: skillData,
+                query: searchInput,
+            });
+        }
+    };
+
+    /** 필터 변경시 데이터 받아오기 */
+    useEffect(() => {
+        getSkillData();
+    }, [filterData]);
+
+    useEffect(() => {
+        getSkillData(true);
+    }, []);
+
+    const [ref, inView] = useInView();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (inView && !loading) {
+            setLoading(true);
+            getSkillData();
+            setLoading(false);
+        }
+    }, [inView]);
     return (
         <>
             <TitlePath title="기술 블로그" path="Menu > 기술블로그" />
